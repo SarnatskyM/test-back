ca7ec8292d9fe99544ccaba7c35cf38684479c0b
Maxim Sarnatsky <sarnatskym@gmail.com>
2023-11-04 22:16:45 +0300

    test back

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/api/metrics', (req, res) => {
+    res.json(СЮДА ОТВЕТ ЗАСУНЬ)
+  });
+
+
+deviceCoordinates = {
+  1234: { lat: 43.032018, lon: 44.674462 },
+};
+
+io.on("connection", (socket) => {
+  console.log("New Websocket Connection");
+
+  socket.on("subscribe", (device) => {
+    socket.device = device;
+    console.log(device);
+
+    if (deviceCoordinates[device]) {
+      console.log(deviceCoordinates[device]);
+      socket.emit("newCoordinates", deviceCoordinates[device]);
+    }
+  });
+
+  socket.on("coordinate", (data) => {
+    setTimeout(() => {
+      const newLat = data.lat;
+      const newLon = data.lon - 0.0001;
+      deviceCoordinates[socket.device] = { lat: newLat, lon: newLon };
+      socket.broadcast.emit("newCoordinates", { lat: newLat, lon: newLon });
+      socket.emit("newCoordinates", { lat: newLat, lon: newLon });
+    }, 2000);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
